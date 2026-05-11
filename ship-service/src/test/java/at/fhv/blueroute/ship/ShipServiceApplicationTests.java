6fd904c9627512826000d7f38d7c8625fd946f4d
Begüm Özkaya <boe1301@students.fhv.at>
2026-05-11 15:13:17 +0200

    Add ship microservice endpoints and voyage integration

---
+++ ship-service/src/test/java/at/fhv/blueroute/ship/ShipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.blueroute.ship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
